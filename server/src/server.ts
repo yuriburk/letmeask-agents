7d3aadb94deaa8440e18bef7beafff5b46c8c5d5
Yuri Burk <yuri.burk@gympass.com>
2025-07-16 17:33:29 -0300

    Added server and biome settings

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,24 @@
-import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { env } from './env.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, { origin: 'http://localhost:5173' })
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get('/health', () => {
+  return 'Ok'
+})
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Server is running on ${env.PORT}`)
+})

f063b121b998ad4163e8ed93d8dfce14eb42fbec
Yuri Burk <yuri.burk@gympass.com>
2025-07-21 21:05:10 -0300

    Update package and schema import

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { env } from '../env.ts'
-import { schema } from '../schema/index.ts'
+import { schema } from './schema/index.ts'
 
 export const sql = postgres(env.DATABASE_URL)
 

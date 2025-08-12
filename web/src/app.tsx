c21f2e62b3523830fb2bde2f1c14c23600763d6c
Yuri Burk <yuri.burk@gympass.com>
2025-08-11 21:04:08 -0300

    Create record room audio component

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
 import { Room } from './pages/room'
 
 const queryClient = new QueryClient()
@@ -13,6 +14,7 @@ export function App() {
         <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="/rooms/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/rooms/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
---
+++ web/src/pages/record-room-audio.tsx
@@ -0,0 +1,95 @@
+import { useRef, useState } from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
+
+const isRecordingSupported =
+  navigator.mediaDevices &&
+  typeof navigator.mediaDevices.getUserMedia === 'function' &&
+  typeof window.MediaRecorder === 'function'
+
+type RoomParams = { roomId: string }
+
+export function RecordRoomAudio() {
+  const [isRecording, setIsRecording] = useState(false)
+  const recorder = useRef<MediaRecorder | null>(null)
+
+  const params = useParams<RoomParams>()
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />
+  }
+
+  async function startRecording() {
+    if (!isRecordingSupported) {
+      alert('O seu navegador não suporta gravação de áudio.')
+      return
+    }
+
+    setIsRecording(true)
+
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44_100,
+      },
+    })
+
+    recorder.current = new MediaRecorder(audio, {
+      mimeType: 'audio/webm',
+      audioBitsPerSecond: 64_000,
+    })
+
+    recorder.current.ondataavailable = (event) => {
+      if (event.data.size > 0) {
+        uploadAudio(event.data)
+      }
+    }
+
+    recorder.current.onstop = () => {
+      for (const track of audio.getTracks()) {
+        track.stop()
+      }
+    }
+
+    recorder.current.start()
+  }
+
+  function stopRecording() {
+    setIsRecording(false)
+
+    if (recorder.current && recorder.current.state !== 'inactive') {
+      recorder.current.stop()
+    }
+  }
+
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData()
+
+    formData.append('file', audio, 'audio.webm')
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      { method: 'POST', body: formData }
+    )
+
+    const result = await response.json()
+
+    return result
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      {isRecording ? (
+        <Button className="min-w-38" onClick={stopRecording}>
+          Parar gravação
+        </Button>
+      ) : (
+        <Button className="min-w-38" onClick={startRecording}>
+          Gravar aúdio
+        </Button>
+      )}
+      <p>{isRecording ? 'Gravando...' : 'Pausado'}</p>
+    </div>
+  )
+}
